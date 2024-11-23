9ab9b0aad6267d2c9b2141156515f65be7b9fdb6
Kunal Kumar <kunalkumar980860@gmail.com>
2024-11-23 17:11:01 +0530

    Add Contentful config and update necessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,29 @@
+require("dotenv").config({ path: "C:\\Users\\kunal\\OneDrive\\Documents\\testdfs\\contentful.env" });
 module.exports = {
   siteMetadata: {
     title: `Gatsby Sydney Ecommerce Theme`,
     siteUrl: `https://jamm.matter.design`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        environment: process.env.CONTENTFUL_ENVIRONMENT
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        // This is the path where the CMS will be available
+        manualInit: true, 
+      },
+    },
+  ],
 };
+console.log('Contentful Space ID:', process.env.CONTENTFUL_SPACE_ID);
+console.log('Contentful Access Token:', process.env.CONTENTFUL_ACCESS_TOKEN);
